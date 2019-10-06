2fefc58227766dbc2c94ef2ef27f1fd637881dbb
Mikhail Zheltyshev <mihalich93@bk.ru>
2019-10-06 09:00:06 +0300

    commit

--- src/test/java/com/searchmodule/tests/SearchTest.java
+++ src/test/java/com/searchmodule/tests/SearchTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import searchmodule.pages.SearchPage;
+import com.searchmodule.pages.SearchPage;
 
 public class SearchTest {
 
